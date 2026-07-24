f87d94d455d9bd65ea221273613af69b1e93b986
B. Charan Kumar Reddy <charankumarreddybantrothula@gmail.com>
2026-07-24 11:31:46 +0530

    Add image upload and grayscale conversion code

--- Lab/1.py
+++ Lab/1.py
@@ -1,1 +1,13 @@
-
+#1
+from google.colab import files
+import cv2
+from matplotlib import pyplot as plt
+uploaded = files.upload()
+import os
+file_name = list(uploaded.keys())[0]
+img = cv2.imread(file_name)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+plt.imshow(gray, cmap='gray')
+plt.title("Gray Image")
+plt.axis('off')
+plt.show()
