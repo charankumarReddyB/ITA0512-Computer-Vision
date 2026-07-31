2636cb44398886edc2a9dd76733e140843329ab6
B. Charan Kumar Reddy <charankumarreddybantrothula@gmail.com>
2026-07-31 14:39:06 +0530

    Add image processing script in Lab/36.py

---
+++ Lab/36.py
@@ -0,0 +1,11 @@
+#36
+from google.colab import files
+import cv2
+import matplotlib.pyplot as plt
+f = list(files.upload().keys())[0]
+img = cv2.imread(f)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+_,mask = cv2.threshold(gray,120,255,cv2.THRESH_BINARY)
+plt.imshow(mask,cmap='gray')
+plt.axis('off')
+plt.show()
